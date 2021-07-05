187530288ad8b16b4be5a9bdcd9174385d119e0d
Oscar Leal <82407333+luziiro@users.noreply.github.com>
2021-07-05 18:49:34 -0400

    Delete forms.py

--- forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Juego
-
-class JuegoForm(ModelForm):
-
-    class Meta:
-        model = Juego
-        fields = ['pegi','nombre','precio','categoria']
